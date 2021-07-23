7e0efcb31024dfade398d31924275529940e6f94
Fenix3123 <63925178+Fenix3123@users.noreply.github.com>
2021-07-23 11:33:22 -0500

    dsds

--- assignment4.java
+++ assignment4.java
@@ -56,14 +56,55 @@ public class assignment4{
          
          
          User user_logged = null;
-         int index;
-         for(index = 0; index < arr_user.length; index++){
+         for(int index = 0; index < arr_user.length; index++){
             if(arr_user[index].getUsername().toUpperCase().equals(username) && arr_user[index].getPassword().equals(password)){
                user_logged = arr_user[index];
                System.out.println("Welcome: "+ arr_user[index].getName());
                System.out.println("----------");
                
                //where the real assignment 4 begins
+               if(arr_user[index].getRole().equals("normal_user")){
+                  while(true){
+        
+                     System.out.println("(0) Update username");
+                     System.out.println("(1) Update password");
+                     System.out.println("(2) Update name");
+                     System.out.println("(3) Exit");
+                     String choice = scanner.nextLine();
+                     
+                     if(choice.equals("0")){
+                        System.out.println("What do you want your new username to be?");
+                        String new_username = scanner.nextLine();
+                        arr_user[index].setUsername(new_username);
+                     }else if(choice.equals("1")){
+                              System.out.println("What do you want your new password to be?");
+                              String new_password = scanner.nextLine();
+                              arr_user[index].setPassword(new_password);
+      
+                     }else if(choice.equals("2")){
+                               System.out.println("What do you want your new Name to be?");
+                               String new_name = scanner.nextLine();
+                               arr_user[index].setName(new_name);
+                     }else if(choice.equals("3")){
+                                Arrays.sort(arr_user);
+                                   try {
+   			                        writer = new BufferedWriter(new FileWriter("users.txt"));
+                                     for(User user : arr_user){
+   			                             writer.write(user.getUsername()+", "+user.getPassword()+", "+user.getName()+", "+user.getRole());
+                                           writer.newLine();
+                                         }
+   		                             } finally {
+   			                                  if (writer != null) writer.close();
+   		                             }
+   
+                                       System.exit(0);
+                      }else{
+                               System.out.println("Please try again");
+                      }
+
+                  }//end of while for normal user
+               
+               }
                while(true){
                   System.out.println("(0) Log in as another user");
                   System.out.println("(1) Update username");
